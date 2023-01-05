48c9b2c90e75178c696f555fb1aa910984634108
Rupali <Rupali@LAPTOP-LQCUHIQ9>
2023-01-05 01:01:34 -1200

    commiting the second time

--- src/gittest/Test.java
+++ src/gittest/Test.java
@@ -3,5 +3,6 @@ package gittest;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("This is sample git project");
+	System.out.println("This is sample2 git project");
 }
 }
